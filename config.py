03d12f12fd167a9365e232d78b4b11808af6be30
Satva39 <satvachauhan11@gmail.com>
2026-07-10 16:26:06 +0530

    comiit-11

--- config.py
+++ config.py
@@ -2,24 +2,19 @@ import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
-
 class Config:
-    # Flask
+
     SECRET_KEY = os.environ.get(
         "SECRET_KEY",
-        "ChangeThisToASecureRandomKey"
+        "ExpoNoteCRMSecret2026"
     )
 
-    # Database
-    # Uses DATABASE_URL on Vercel, falls back to SQLite locally
     SQLALCHEMY_DATABASE_URI = os.environ.get(
-        "DATABASE_URL",
-        "sqlite:///" + os.path.join(BASE_DIR, "database", "expo.db")
+        "DATABASE_URL"
     )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    # Uploads
     UPLOAD_FOLDER = os.path.join(
         BASE_DIR,
         "static",
