fcdb29e434af552b40cd0d52adbf5f240f0f61d7
Satva39 <satvachauhan11@gmail.com>
2026-07-10 16:28:42 +0530

    commit-12

--- app.py
+++ app.py
@@ -30,6 +30,9 @@ app.config.from_object(Config)
 
 db.init_app(app)
 
+with app.app_context():
+    db.create_all()
+
 # Create upload folder only when running locally
 if __name__ == "__main__":
     os.makedirs(
