d4f348457174ce7f7ec9189327849686c8066793
Satva39 <satvachauhan11@gmail.com>
2026-07-19 10:56:17 +0530

    commit-23

--- app.py
+++ app.py
@@ -29,6 +29,13 @@ import cloudinary
 import cloudinary.uploader
 import cloudinary.api
 
+cloudinary.config(
+    cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
+    api_key=os.getenv("CLOUDINARY_API_KEY"),
+    api_secret=os.getenv("CLOUDINARY_API_SECRET"),
+    secure=True
+)
+
 # --------------------------------------------------
 # App Configuration
 # --------------------------------------------------
